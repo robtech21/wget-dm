e80e2e3ff16646f1cafcd072cc166904c1f9eb21
Robert Furr <77758464+robtech21@users.noreply.github.com>
2021-03-29 11:06:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,15 @@
 import os
 from os import system
 
+pnt   = print
+inpt  = input
+
 if os.name == 'nt':
   print('Nope')
   exit(1)
   
 def clr():
   system('clear')
+
+while True:
+  print('Coming soon')
